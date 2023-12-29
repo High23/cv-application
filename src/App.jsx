3f24ceae30829f1782e6b4ef1ce7e5573740f1ed
High23 <warenasb2@gmail.com>
2023-12-29 16:43:44 -0700

    Added cv preview elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  /* Use state here to update cv preview with the inputted info from inputs 
+    pass setState through inputs for the state to update on next render
+  */
   return (
     <main>
       <form action="/">
         <section id='general-information'>
+          <h2>Personal Information</h2>
           <Input labelDesc={'name'} type={'text'}></Input>
           <Input labelDesc={'email'} type={'email'}></Input>
           <Input labelDesc={'phone number'} type={'tel'}></Input>
         </section>
         <section id='educational-experience'>
+          <h2>Education</h2>
           <Input labelDesc={'school name'} type={'text'}></Input>
           <Input labelDesc={'title of study'} type={'text'}></Input>
           <Input labelDesc={'study completion date'} type={'datetime-local'}></Input>
-          </section>
+        </section>
         <section id='practical-experience'>
+          <h2>Work Experience</h2>
           <Input labelDesc={'company name'} type={'text'}></Input>
           <Input labelDesc={'position title'} type={'text'}></Input>
           <Input labelDesc={'start date'} type={'text'}></Input>
@@ -24,8 +30,45 @@ function App() {
         </section>
       </form>
       <section id='cv-preview'>
-        <div className='cv-right'></div>
-        <div className='cv-left'></div>
+        <div className='cv-header'>
+          <div>
+            <span>Name</span>
+          </div>
+          <div>
+            <span>Email</span>
+          </div>
+          <div>
+            <span>Phone Number</span>
+          </div>
+          <img src="" alt="" />
+        </div>
+        <div className='cv-body'>
+          <h2>Education</h2>
+          <div>
+            <span>School Name</span>
+          </div>
+          <div>
+            <span>Title of Study</span>
+          </div>
+          <div>
+            <span>Completion Date</span>
+          </div>
+        </div>
+        <div className='cv-footer'>
+          <h2>Work Experience</h2>
+          <div>
+            <span>Company Name</span>
+          </div>
+          <div>
+            <span>Position Title</span>
+          </div>
+          <div>
+            <span>Start date - End date</span>
+          </div>
+          <div>
+            <span>Description</span>
+          </div>
+        </div>
       </section>
     </main>
   )
