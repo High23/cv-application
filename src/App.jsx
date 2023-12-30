6c3058b541df100c77852dc97e678b9bb0b548bf
High23 <warenasb2@gmail.com>
2023-12-29 19:03:10 -0700

    CV preview now updates as info is being inputted

--- src/App.jsx
+++ src/App.jsx
@@ -5,92 +5,85 @@ function App() {
   /* Use state here to update cv preview with the inputted info from inputs 
     pass setState through inputs for the state to update on next render
   */
+
+  const [personalInformation, setpersonalInformation] =  useState({
+    fullName: '', email: '', phoneNumber: '', 
+    schoolName: '', studyTitle: '', completionDate: '', 
+    companyName: '', positionTitle: '', startDate: '', endDate: '', 
+    description: ''
+  })
+
+  function updateStateObj(property, element) {
+    setpersonalInformation({...personalInformation, [[property]]: element.target.value})
+  }
+
+
   return (
     <main>
       <form action="/">
         <section id='general-information'>
           <h2>Personal Information</h2>
-          <Input labelDesc={'name'} type={'text'}></Input>
-          <Input labelDesc={'email'} type={'email'}></Input>
-          <Input labelDesc={'phone number'} type={'tel'}></Input>
+          <Input labelDesc={'full name'} type={'text'} onChange={(element) => { updateStateObj('fullName', element )}} ></Input>
+          <Input labelDesc={'email'} type={'email'} onChange={(element) => { updateStateObj('email', element )}} ></Input>
+          <Input labelDesc={'phone number'} type={'tel'} onChange={(element) => { updateStateObj('phoneNumber', element )}} ></Input>
         </section>
         <section id='educational-experience'>
           <h2>Education</h2>
-          <Input labelDesc={'school name'} type={'text'}></Input>
-          <Input labelDesc={'title of study'} type={'text'}></Input>
-          <Input labelDesc={'study completion date'} type={'datetime-local'}></Input>
+          <Input labelDesc={'school name'} type={'text'} onChange={(element) => { updateStateObj('schoolName', element )}} ></Input>
+          <Input labelDesc={'title of study'} type={'text'} onChange={(element) => { updateStateObj('studyTitle', element )}} ></Input>
+          <Input labelDesc={'study completion date'} type={'datetime-local'} onChange={(element) => { updateStateObj('completionDate', element )}} ></Input>
         </section>
         <section id='practical-experience'>
           <h2>Work Experience</h2>
-          <Input labelDesc={'company name'} type={'text'}></Input>
-          <Input labelDesc={'position title'} type={'text'}></Input>
-          <Input labelDesc={'start date'} type={'text'}></Input>
-          <Input labelDesc={'end date'} type={'text'}></Input>
-          <TextArea labelDesc={'job description'}></TextArea>
+          <Input labelDesc={'company name'} type={'text'} onChange={(element) => { updateStateObj('companyName', element )}} ></Input>
+          <Input labelDesc={'position title'} type={'text'} onChange={(element) => { updateStateObj('positionTitle', element )}} ></Input>
+          <Input labelDesc={'start date'} type={'text'} onChange={(element) => { updateStateObj('startDate', element )}} ></Input>
+          <Input labelDesc={'end date'} type={'text'} onChange={(element) => { updateStateObj('endDate', element )}} ></Input>
+          <TextArea labelDesc={'job description'} onChange={(element) => { updateStateObj('description', element )}} ></TextArea>
         </section>
       </form>
       <section id='cv-preview'>
         <div className='cv-header'>
-          <div>
-            <span>Name</span>
-          </div>
-          <div>
-            <span>Email</span>
-          </div>
-          <div>
-            <span>Phone Number</span>
-          </div>
-          <img src="" alt="" />
+          <span>{personalInformation.fullName}</span>          
+          <span>{personalInformation.email}</span>
+          <span>{personalInformation.phoneNumber}</span>
+          <img src="src/assets/account-circle.png" alt="" />
         </div>
         <div className='cv-body'>
           <h2>Education</h2>
-          <div>
-            <span>School Name</span>
-          </div>
-          <div>
-            <span>Title of Study</span>
-          </div>
-          <div>
-            <span>Completion Date</span>
-          </div>
+          <span>{personalInformation.schoolName}</span>
+          <span>{personalInformation.studyTitle}</span>
+          <span>{personalInformation.completionDate}</span>
         </div>
         <div className='cv-footer'>
-          <h2>Work Experience</h2>
-          <div>
-            <span>Company Name</span>
-          </div>
-          <div>
-            <span>Position Title</span>
-          </div>
-          <div>
-            <span>Start date - End date</span>
-          </div>
-          <div>
-            <span>Description</span>
-          </div>
+          <h2>Work Experience</h2>          
+          <span>{personalInformation.companyName}</span>          
+          <span>{personalInformation.positionTitle}</span>
+          <span>{personalInformation.startDate +  '-' + personalInformation.endDate}</span>
+          <span>{personalInformation.description}</span>
         </div>
       </section>
     </main>
   )
 }
 
-function Input({labelDesc, type}) {
+function Input({labelDesc, type, onChange}) {
   let desc = labelDesc.split(' ').join('-')
   labelDesc = capitalize(labelDesc)
 
   return (
     <>
-      <label htmlFor={desc}>{labelDesc + ':'} </label>
-      <input type={type} id={desc}/>
+      <label htmlFor={desc}>{labelDesc} </label>
+      <input type={type} name={desc} id={desc} onChange={onChange}/>
     </>
   )
 }
 
-function TextArea({labelDesc}) {
+function TextArea({labelDesc, onChange}) {
   return (
     <>
       <label htmlFor={labelDesc}>{labelDesc} </label>
-      <textarea id={labelDesc} cols='30' rows='10' ></textarea>
+      <textarea id={labelDesc} cols='30' rows='10' onChange={onChange}></textarea>
     </>
   )
 }
